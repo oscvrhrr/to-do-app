51a13750fde7c7221c9b30131a83f0479c9b0e2a
Oscar Herrera <a.oscarherrera@gmail.com>
2024-02-14 09:10:52 -0600

    input forms, fields

--- src/app/board-widgets.js
+++ src/app/board-widgets.js
@@ -32,24 +32,66 @@ function boardForm() {
   boardNameInput.id = "board-name";
   form.appendChild(boardInputLabel)
   form.appendChild(boardNameInput)
+  boardNameInput.setAttribute('required', true)
 
   const boardColumnLabel = document.createElement('label'),
-  boardColumnInput = document.createElement('input');
+  boardColumnInput = document.createElement('select');
+  const boardColumnTodo = document.createElement('option');
+  const boardColumnDoing = document.createElement('option');
+  const boardColumnDone = document.createElement('option');
+
   boardColumnInput.id = 'board-column';
+  boardColumnInput.appendChild(boardColumnTodo);
+  boardColumnInput.appendChild(boardColumnDoing);
+  boardColumnInput.appendChild(boardColumnDone);
+
+  boardColumnTodo.textContent = 'ToDo';
+  boardColumnDoing.textContent = 'Doing';
+  boardColumnDone.textContent = 'Done';
 
 
   boardColumnLabel.innerText = 'Board Columns'
   form.appendChild(boardColumnLabel)
   form.appendChild(boardColumnInput)
-
   const addNewColumnBtn = document.createElement('div'),
   createNewBoardBtn = document.createElement('button');
   createNewBoardBtn.setAttribute('type', 'submit');
   addNewColumnBtn.innerText = '+ Add New Column'
   createNewBoardBtn.innerText = 'Create New Board'
   createNewBoardBtn.classList.add('create-new-board-btn')
+    
+  let currentColumns = 1
+  const maxColumns = 3;
+  addNewColumnBtn.addEventListener('click', () => {
+
+    if(currentColumns < maxColumns) {
+    const newbtn = document.createElement('select');
+    
+    const columnTodo = document.createElement('option');
+    const columnDoing = document.createElement('option');
+    const columnDone = document.createElement('option');
+
+    newbtn.appendChild(columnTodo);
+    newbtn.appendChild(columnDoing);
+    newbtn.appendChild(columnDone);
+
+    columnTodo.textContent = 'ToDo';
+    columnDoing.textContent = 'Doing';
+    columnDone.textContent = 'Done';
+
+    boardColumnInput.parentNode.insertBefore(newbtn, boardColumnInput.nextSibling);
+    currentColumns++
+
+    } else if(currentColumns === maxColumns) {
+      return alert('Max Columns Reached')
+    }
+    
+
+  })
+  
   form.appendChild(addNewColumnBtn)
   form.appendChild(createNewBoardBtn)
+  
 
 
   document.body.appendChild(form)
